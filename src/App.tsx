b99c538dc78485372e9b7b2da61877094017acfd
Ivan Ticona <ivan.ticona.v@gmail.com>
2026-02-14 18:41:45 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="main-container">
+      <section className="card">
+        <h1 className="title">
+          Coming soon
+        </h1>
+        <p className="description">
+          Estoy construyendo mi portfolio. Vuelve pronto 🙂
+        </p>
+        <div className="badge">
+          🚧 En construcción
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
